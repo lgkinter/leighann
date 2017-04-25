d7b1173d3e8617a6dcef6ca86cb6f41fcdcc77e4
lgkinter <lgkinter@yahoo.com>
2017-04-25 12:43:32 -0400

    finish creating project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @contact = Contact.new
@@ -23,14 +24,31 @@ class ProjectsController < ApplicationController
     @project = Project.friendly.find(params[:id])
   end
 
+  def edit
+    @project = Project.friendly.find(params[:id])
+  end
+
+  def update
+    @project = Project.friendly.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:alert] = "Project has been successfully updated."
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
+    Project.friendly.find(params[:id]).destroy
+    flash[:alert] = "Project has been successfully deleted."
+    redirect_to root_path
   end
 
   private
 
     def project_params
       params.require(:project).permit(:title, :subtitle, :index_pic, :date, :skills,
-      :link, :overview, profile_pics: [])
+      :link, :overview, :categories, :slug, profile_pics: [])
     end
 
 end
