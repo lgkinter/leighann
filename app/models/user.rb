2c626326ba12591cc3c925bbfd5f66064945fdc6
lgkinter <lgkinter@yahoo.com>
2017-04-26 20:48:49 -0400

    add devise registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable,
           :trackable, :validatable
 
   def is_admin?
