41679ba902c37b44ef751fe634e19e4b4e934c05
lgkinter <lgkinter@yahoo.com>
2017-05-13 13:10:37 -0400

    flesh out letterings model

--- app/controllers/letterings_controller.rb
+++ app/controllers/letterings_controller.rb
@@ -1,2 +1,44 @@
 class LetteringsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @lettering = Lettering.new
+  end
+
+  def create
+    @lettering = Lettering.new(lettering_params)
+    if @lettering.save
+      flash[:alert] = "Lettering created!"
+      redirect_to "new"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @lettering = Lettering.find(params[:id])
+  end
+
+  def update
+    @lettering = Lettering.find(params[:id])
+    if @lettering.update_attributes(lettering_params)
+      flash[:alert] = "Lettering has been successfully updated."
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Lettering.find(params[:id]).destroy
+    flash[:alert] = "Lettering has been successfully deleted."
+    redirect_to root_path
+  end
+
+  private
+
+    def lettering_params
+      params.require(:lettering).permit(:name, :image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: "registrations"}
   resources :projects
+  resources :letterings
   resources :contacts, only: [:new, :create]
   get "/smallbusiness" => "projects#sbed"
 
