6fa25df86bbd6250be8d942cba894203e5d0de8c
lgkinter <lgkinter@yahoo.com>
2017-05-14 02:29:25 -0400

    precompile assets

--- app/controllers/letterings_controller.rb
+++ app/controllers/letterings_controller.rb
@@ -9,7 +9,7 @@ class LetteringsController < ApplicationController
     @lettering = Lettering.new(lettering_params)
     if @lettering.save
       flash[:alert] = "Lettering created!"
-      redirect_to "new"
+      redirect_to "letterings#new"
     else
       render "new"
     end
